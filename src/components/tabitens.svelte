f9acc2e8e366de2a28ec9d2d19c4653e48608702
Marcell Santana de Deus <marcellsantana@gmail.com>
2022-05-04 08:55:00 -0300

    Adding styles

--- src/components/tabitens.svelte
+++ src/components/tabitens.svelte
@@ -1,13 +1,19 @@
-{#each tabNames as tabName, index}
-<button
-class={index === currentTab ? 'selected' : ''}
-on:click={e=>onSelection(index)}>{tabName}</button>
-{/each}
-<h2>{tabTexts[currentTab]}</h2>
+<div>
+    {#each tabNames as tabName, index}
+        <button
+            class={index === currentTab ? 'selected' : ''}
+            on:click={e=>onSelection(index)}>{tabName}
+        </button>
+    {/each}
+</div>
+
+<div class="text">
+    <h2>{tabTexts[currentTab]}</h2>
+</div>
+
 
 <script lang="ts">
     import {createEventDispatcher} from 'svelte'
-
     export let tabNames: string[] = []
     export let tabTexts: string[] = []
     export let currentTab = undefined
@@ -18,7 +24,34 @@ on:click={e=>onSelection(index)}>{tabName}</button>
 </script>
 
 <style>
-    .selected{
-        font-weight: bold;
+
+    button {
+    
+    margin-left:10px;
+    background-color: #ccc;
+    border: 1px solid rgb(118, 118, 118);
+    border-radius: 5px;
+    padding: 2px 5px;
+    font-size: 15pt;
+    align-items: center;
+    }
+
+    .selected {
+    font-weight: bold;
+    background-color: #aaa;
+    }
+
+
+    .text{
+    background-color: #ccc;
+    font-weight: normal;
+    font-size: 12pt;
+    text-align: left;
+    margin-top: 15px;
+    margin-left: 15px;
+    width:200pt;
+    height: 100pt;
+    border-radius: 10px;
+    color:black
     }
 </style>
